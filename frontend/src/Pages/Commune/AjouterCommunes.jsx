2cd5f9768f1f8b66c9bc192c41e1dca40773e258
BellaNH <bouchranahnah@gmail.com>
2025-07-30 12:40:15 +0100

    npm run build

--- frontend/src/Pages/Commune/AjouterCommunes.jsx
+++ frontend/src/Pages/Commune/AjouterCommunes.jsx
@@ -60,11 +60,7 @@ const AjouterCommunes = () => {
         <Typography variant="h6" gutterBottom>
           Ajouter une nouvelle commune
         </Typography>
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-        >
+     
           <TextField
             label="Nom de la commune"
             value={nom}
